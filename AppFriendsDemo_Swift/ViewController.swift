1c96451c8609548572ed623b46c4302a63523e09
Hao Wang <wang.hao.chnbj@gmail.com>
2016-03-11 00:12:35 -0500

    initial commit

---
+++ AppFriendsDemo_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppFriendsDemo_Swift
+//
+//  Created by HAO WANG on 3/11/16.
+//  Copyright © 2016 Hacknocraft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
